1682be5c62d2488b96d9289726e37ae92f87a974
Ryotaro Banno <ryotaro.banno@gmail.com>
2024-06-11 07:34:39 +0000

    remove useless constants
    Signed-off-by: Ryotaro Banno <ryotaro.banno@gmail.com>

--- constants/constants.go
+++ constants/constants.go
@@ -1,14 +1,11 @@
 package constants
 
 const (
-	ProbeNamePrefix           = "pie-probe"
-	ProvisionProbeNamePrefix  = "provision"
-	MountProbeNamePrefix      = "mount"
-	ProbeContainerName        = "probe"
-	PodFinalizerName          = "pie.topolvm.io/pod"
-	NodeFinalizerName         = "pie.topolvm.io/node"
-	StorageClassFinalizerName = "pie.topolvm.io/storage-class"
-	PVCNamePrefix             = "pie-pvc"
+	ProvisionProbeNamePrefix = "provision"
+	MountProbeNamePrefix     = "mount"
+	ProbeContainerName       = "probe"
+	PodFinalizerName         = "pie.topolvm.io/pod"
+	PVCNamePrefix            = "pie-pvc"
 
 	ProbeNodeLabelKey         = "node"
 	ProbeStorageClassLabelKey = "storage-class"
