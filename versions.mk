f24b9ed89802f3d43f757e77a33791e500b879ef
Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>
2024-10-24 09:03:05 +0000

    Support kubernetes 1.31
    Also drop 1.28 support.
    
    Signed-off-by: Toshikuni Fukaya <toshikuni-fukaya@cybozu.co.jp>

--- versions.mk
+++ versions.mk
@@ -1,16 +1,16 @@
 # https://github.com/helm/chart-testing/releases
 CHART_TESTING_VERSION := 3.11.0
 # https://github.com/kubernetes-sigs/controller-tools/releases
-CONTROLLER_TOOLS_VERSION := v0.15.0
+CONTROLLER_TOOLS_VERSION := v0.16.4
 # https://github.com/helm/helm/releases
-HELM_VERSION := 3.15.2
+HELM_VERSION := 3.16.2
 # https://github.com/kubernetes-sigs/kind/releases
-KIND_VERSION := v0.23.0
+KIND_VERSION := v0.24.0
 # https://github.com/kubernetes-sigs/kustomize/releases
-KUSTOMIZE_VERSION := v5.4.2
+KUSTOMIZE_VERSION := v5.5.0
 
 # It is set by CI using the environment variable, use conditional assignment.
-KUBERNETES_VERSION ?= 1.30
+KUBERNETES_VERSION ?= 1.31
 
 # Tools versions which are defined in go.mod
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
@@ -22,9 +22,9 @@ ENVTEST_K8S_VERSION := $(KUBERNETES_VERSION).x
 
 # The container version of kind must be with the digest.
 # ref. https://github.com/kubernetes-sigs/kind/releases
-KIND_NODE_VERSION := kindest/node:v1.30.0@sha256:047357ac0cfea04663786a612ba1eaba9702bef25227a794b52890dd8bcd692e
-ifeq ($(KUBERNETES_VERSION), 1.29)
-	KIND_NODE_VERSION := kindest/node:v1.29.4@sha256:3abb816a5b1061fb15c6e9e60856ec40d56b7b52bcea5f5f1350bc6e2320b6f8
-else ifeq ($(KUBERNETES_VERSION), 1.28)
-	KIND_NODE_VERSION := kindest/node:v1.28.9@sha256:dca54bc6a6079dd34699d53d7d4ffa2e853e46a20cd12d619a09207e35300bd0
+KIND_NODE_VERSION := kindest/node:v1.31.0@sha256:53df588e04085fd41ae12de0c3fe4c72f7013bba32a20e7325357a1ac94ba865
+ifeq ($(KUBERNETES_VERSION), 1.30)
+	KIND_NODE_VERSION := kindest/node:v1.30.4@sha256:976ea815844d5fa93be213437e3ff5754cd599b040946b5cca43ca45c2047114
+else ifeq ($(KUBERNETES_VERSION), 1.29)
+	KIND_NODE_VERSION := kindest/node:v1.29.8@sha256:d46b7aa29567e93b27f7531d258c372e829d7224b25e3fc6ffdefed12476d3aa
 endif
